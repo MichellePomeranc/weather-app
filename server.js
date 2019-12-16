6b8c9530543090bfaa22747a1d9b58bd321072b2
Michelle <michellepomeranc@gmail.com>
2019-12-16 11:39:09 +0200

    fixed bug

--- server.js
+++ server.js
@@ -7,18 +7,20 @@ const path = require('path')
 const mongoose = require('mongoose')
 const bodyParser = require("body-parser")
 
-app.use(express.static(path.join(__dirname, '../dist')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(express.static(path.join(__dirname, '/dist')))
+app.use(express.static(path.join(__dirname, '/node_modules')))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', api)
 
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/weatherDB', {
+// process.env.MONGODB_URI||
+mongoose.connect('mongodb://localhost/weatherDB', {
     useNewUrlParser: true
 })
 
-const port = process.env.PORT || 3000
+const port = 3000
+// process.env.PORT ||
 app.listen(port, function () {
     console.log(`running server on port ${port}`)
 })
